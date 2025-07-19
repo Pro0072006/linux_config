a6113e9e4fde74658290254b3d370d4d45e964ea
Santiago Mendoza <santiagoyasno@gmail.com>
2025-07-18 23:59:35 -0500

    Create user.js

---
+++ apps/user.js
@@ -0,0 +1,10 @@
+user_pref("full-screen-api.warning.timeout", 0);
+user_pref("full-screen-api.transition-duration.enter", "0 0");
+user_pref("full-screen-api.transition-duration.leave", "0 0");
+
+user_pref("browser.urlbar.trimURLs", false);
+
+user_pref("media.autoplay.default", 2);
+
+user_pref("privacy.trackingprotection.enabled", true);
+user_pref("privacy.trackingprotection.socialtracking.enabled", true);
